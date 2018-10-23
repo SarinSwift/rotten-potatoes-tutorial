857178ca7e327c506ed7f219215845c829ef71a9
SarinSwift <sarinyaswift@gmail.com>
2018-10-22 22:31:03 -0700

    Users can see all reviews

--- app.js
+++ app.js
@@ -4,9 +4,19 @@ var exphbs = require('express-handlebars');
 const express = require('express')
 const app = express()
 
+// mock array of movies
+let reviews = [
+    { title: "Great review", movieTitle: "Titanic" },
+    { title: "Awesome movie", movieTitle: "Interstellar" }
+]
+
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+// we're sending the mock array as an object in the template { reviews: reviews }
+app.get('/', (req, res) => {
+    res.render('reviews-index', { reviews: reviews });
+})
 
 app.get('/', (req, res) => {
     // extending the root route ('/') to render home.handlebars
